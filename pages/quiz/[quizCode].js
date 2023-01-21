9d0b2c4284f3644881c9634dc1b6c9d591c9cf37
Talal Ahmed <talalahmed252@gmail.com>
2023-01-21 18:29:50 +0500

    Fixed Session Issue

--- pages/quiz/[quizCode].js
+++ pages/quiz/[quizCode].js
@@ -62,22 +62,105 @@ function Quiz() {
   useEffect(() => {
     window.addEventListener('blur', () => {
       if(startQuiz) {
-        alert("TRYING TO CHEAT???????????");
         setStartQuiz(false);
       }
     });
     document.addEventListener('fullscreenchange', () => {
-      if(!document.fullscreenElement && startQuiz) {
-        alert("TRYING TO CHEAT???????????");
+      if(!document.fullscreenElement) {
         setStartQuiz(false);
       }
     });
-    document.addEventListener('visibilitychange', () => {
-      if(document.visibilityState === 'hidden' && startQuiz) {
-        alert("TRYING TO CHEAT???????????");
-        setStartQuiz(false);
+
+    var browserPrefixes = ['moz', 'ms', 'o', 'webkit'],
+    isVisible = true; // internal flag, defaults to true
+
+// get the correct attribute name
+function getHiddenPropertyName(prefix) {
+  return (prefix ? prefix + 'Hidden' : 'hidden');
+}
+
+// get the correct event name
+function getVisibilityEvent(prefix) {
+  return (prefix ? prefix : '') + 'visibilitychange';
+}
+
+// get current browser vendor prefix
+function getBrowserPrefix() {
+  for (var i = 0; i < browserPrefixes.length; i++) {
+    if(getHiddenPropertyName(browserPrefixes[i]) in document) {
+      // return vendor prefix
+      return browserPrefixes[i];
+    }
+  }
+
+  // no vendor prefix needed
+  return null;
+}
+
+// bind and handle events
+var browserPrefix = getBrowserPrefix(),
+    hiddenPropertyName = getHiddenPropertyName(browserPrefix),
+    visibilityEventName = getVisibilityEvent(browserPrefix);
+
+function onVisible() {
+if(isVisible) {
+    return;
+  }
+ 
+  // change flag value
+  isVisible = true;
+  console.log('visible');
+}
+
+function onHidden() {
+  // prevent double execution
+  if(!isVisible) {
+    return;
+  }
+
+  // change flag value
+  isVisible = false;
+  setStartQuiz(false);
+  console.log('hidden');
+}
+
+  function handleVisibilityChange(forcedFlag) {
+    // forcedFlag is a boolean when this event handler is triggered by a
+    // focus or blur eventotherwise it's an Event object
+    if(typeof forcedFlag === "boolean") {
+      if(forcedFlag) {
+        return onVisible();
       }
-    });
+
+      return onHidden();
+    }
+    if(document[hiddenPropertyName]) {
+      return onHidden();
+    }
+
+    return onVisible();
+  }
+
+  document.addEventListener(visibilityEventName, handleVisibilityChange, false);
+
+  // extra event listeners for better behaviour
+  document.addEventListener('focus', function() {
+    handleVisibilityChange(true);
+  }, false);
+
+  document.addEventListener('blur', function() {
+    handleVisibilityChange(false);
+  }, false);
+
+  window.addEventListener('focus', function() {
+      handleVisibilityChange(true);
+  }, false);
+
+  window.addEventListener('blur', function() {
+    handleVisibilityChange(false);
+  }, false);
+
+
   }, []);
   return (
     <div className="container-fluid pt-5 h-100">
