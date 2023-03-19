a0d79547ddf14b043459817288f1dc3c02c37b36
Ahmed Bilal <ahmedbilal2015@gmail.com>
2023-03-19 18:29:09 +0500

    Added Question Generation from PDF,DOCX

--- pages/mcq_quiz_form.js
+++ pages/mcq_quiz_form.js
@@ -1,28 +1,46 @@
-import { useEffect, useState } from "react"
-
+import { useEffect, useState} from "react"
 import QWizard_logo from "./logo"
-import Dropzone from 'react-dropzone'
-import MCQ_Questions from "./mcq_questions"
+import {Dropzone} from 'react-dropzone'
 import Link from 'next/link';
+import axios from "axios"
+import MCQ_Questions from "./mcq_qa/mcq_questions";
 
 
 export default function MCQ_Quiz_form() {
-    const [passage, setPassage] = useState('')
+    const url ="http://localhost:8080/"
+    const [passage, setPassage] = useState("")
     const [generate, setGenerate] = useState(false)
     
+    const onChangeHandler= async event=>{
+        // setFile(event.target.files[0])
+        // console.log(event.target.files[0])
+        const data = new FormData;
+        data.append('uploaded_file',event.target.files[0])
+        var response = {}
+        if(event.target.files[0].type == "application/pdf"){
+            response = await axios.post(url+'read_PDF', data)
+        }
+        else{
+            response = await axios.post(url+'read_DOCX', data)
+        }
+        setPassage(response.data)
+        setGenerate(false)
+    }
+
     useEffect(()=> {
     }, [generate])
 
   
     return (
         <>
-        <QWizard_logo></QWizard_logo> 
+        <QWizard_logo></QWizard_logo>
         <div class='container'>
             <div className="row mb-3 justify-content-space-between">
-                <div className="col-9">
-                    <h3>Generate Multiple Choice Questions</h3>
+                <div className="col-10">
+                {/* <small><span style={{'color':'orange'}}>paste a sample text to generate questions</span></small> */}
+                    <h3 class>Generate Multiple Choice Questions</h3>
                 </div>
-                <div className="col-3">
+                <div className="col-2">
                     <button type="button" class="btn btn-orange"><Link href="/current_quiz">Check out Quiz</Link></button>
                 </div>
             </div>
@@ -31,37 +49,44 @@ export default function MCQ_Quiz_form() {
                         fontSize: 'larger',
                         color: 'orange'
                     }}>
-                        Paste a sample text to generate Multiple Choice Questions
+                        Paste a sample text to generate Questions
                 </div> 
             </div>
-            <div style={{'display': 'flex','justifyContent':'space-between'}}>            
+
+            <div style={{'display': 'flex','justifyContent':'space-between'}}>
+            
             </div>
             <form>
                 <div class="form-group" style={{'display':"flex", justifyContent: "space-between"}}>
                     <label for="exampleFormControlTextarea1"></label>
                     <textarea  style={{"maxWidth":"100%"}} class="form-control" id="exampleFormControlTextarea1" rows="10" cols="50" onChange={e => {setPassage(e.target.value)
-                    console.log('value', e.target.value)
+                    console.log('value', passage)
                     setGenerate(false)}}></textarea>
-
-                {/* <h3><span style={{'color':'orange'}}>Or</span></h3>
-                
-                <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
-                {({getRootProps, getInputProps}) => (
-                    <section>
-                    <div {...getRootProps()} style={{'border':"2px solid black", 'borderStyle':"dashed", "height":"100%"}}>
-                        <input {...getInputProps()} />
-                        <h6 style={{'color':'orange'}}>Drag 'n' drop some files here, or click to select files</h6>
+                        <div class="col-md-6">
+                            <div class="form-group files color">
+                            {/* <label>Upload Your File </label> */}
+                            <input type="file" class="form-control" multiple="" onChange={onChangeHandler}></input>
+                        </div>
                     </div>
-                    </section>
-                )}
-                </Dropzone> */}
-
-                </div>
+	            </div>
             </form>
-            <div className="row justify-content-center">
-                <button type="button col-12" class="btn btn-orange w-50" onClick={() => setGenerate(true)}>Go!</button>
+            <div className="container">
+            <span class="border"> 
+                <div className="row g-1">
+                    <div className="col-6">
+                        <button class="btn btn-orange w-100" onClick={() => setGenerate(true)}>Generate using Text</button>
+                    </div>
+                    <div className="col-6">
+                        <button class="btn btn-orange w-100" onClick={() => {
+                            setGenerate(true)
+                        }}>Generate using File</button>
+                    </div>
+                </div>
+            </span> 
             </div>
+            
             {generate ? <MCQ_Questions content={passage}></MCQ_Questions>:null}
+
         </div>
         </>
         
