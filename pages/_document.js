84073bbed1fe7b1a8e58f9c6e742aea2e0ca05bd
Talal Ahmed <talalahmed252@gmail.com>
2023-04-10 23:41:27 +0500

    Updates in UI

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,9 @@ export default function Document() {
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous" />
       <link rel="stylesheet" href="https://drive.google.com/uc?export=view&id=1yTLwNiCZhIdCWolQldwq4spHQkgZDqkG" />
       <link rel="shortcut icon" href="/logo.svg" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;600;800&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
--- pages/login.js
+++ pages/login.js
@@ -8,6 +8,7 @@ import { toast } from "react-toastify";
 import {userConverter} from '../components/User';
 import User from '../components/User';
 import { setDoc, doc, getDoc } from 'firebase/firestore';
+import styles from '../styles/login.module.css';
 
 function Login() {
   const googleAuth = new GoogleAuthProvider();
@@ -54,43 +55,79 @@ function Login() {
 
 
   return (
-    <div className='container-md mt-5'>
-        <div className="row justify-content-center">
-          <div className='p-4 col-sm-8 col-md-6 rounded-div login-div'>
-              <div className='d-flex justify-content-center'>
-                <Image src="/logo.svg" className='mx-auto' alt="Vercel Logo" width={250} height={250} />
-              </div>
-              {
-                loading && user ? 
-                <section>
-                  <div className='d-flex justify-content-center'>
-                    <Image src={user.photoURL} className='mx-auto' alt="Qwizard Logo" width={100} height={100} />
-                  </div>
-                  <div className='d-flex mt-3'>
-                    <p className='mx-auto lead'>Welcome, {user.displayName}<br></br>Email: {user.email}</p>
-                  </div>
-                  <div className='d-flex justify-content-center'>
-                    <Image src="/loader.svg" className='mx-auto' alt="Profile Logo" width={50} height={50} />
-                  </div>
-                </section>
-                :
-                <section>
-                  <div className='d-flex'>
-                    <p className='lead'>Qwizard  is a Learning Management System (LMS) which incorporates inside itself:<br></br>
-                      🌎Automatic Question Generation (Short Answer + MCQs)<br></br>
-                      🌎Automatic Solution (Answer) Generation<br></br>
-                      🌎Automatic Quiz Marking.<br></br>
-                      🌎Real-time Quiz Attempting Session for students.
-                    </p>
-                  </div>
-                  <div className='d-flex mt-5 mb-3'>
-                  <button className='mx-auto btn btn-legendary' onClick={loginFunction}>Login / SignUp <i className="fa fa-google" aria-hidden="true"></i></button>
-                  </div>
-                </section>
-              }
-          </div>
-        </div>
+    // <div className='container-md mt-5'>
+    //     <div className="row justify-content-center">
+    //       <div className='p-4 col-sm-8 col-md-6 rounded-div login-div'>
+    //           <div className='d-flex justify-content-center'>
+    //             <Image src="/logo.svg" className='mx-auto' alt="Vercel Logo" width={250} height={250} />
+    //           </div>
+    //           {
+    //             loading && user ? 
+    //             <section>
+    //               <div className='d-flex justify-content-center'>
+    //                 <Image src={user.photoURL} className='mx-auto' alt="Qwizard Logo" width={100} height={100} />
+    //               </div>
+    //               <div className='d-flex mt-3'>
+    //                 <p className='mx-auto lead'>Welcome, {user.displayName}<br></br>Email: {user.email}</p>
+    //               </div>
+    //               <div className='d-flex justify-content-center'>
+    //                 <Image src="/loader.svg" className='mx-auto' alt="Profile Logo" width={50} height={50} />
+    //               </div>
+    //             </section>
+    //             :
+    //             <section>
+    //               <div className='d-flex'>
+    //                 <p className='lead'>Qwizard  is a Learning Management System (LMS) which incorporates inside itself:<br></br>
+    //                   🌎Automatic Question Generation (Short Answer + MCQs)<br></br>
+    //                   🌎Automatic Solution (Answer) Generation<br></br>
+    //                   🌎Automatic Quiz Marking.<br></br>
+    //                   🌎Real-time Quiz Attempting Session for students.
+    //                 </p>
+    //               </div>
+    //               <div className='d-flex mt-5 mb-3'>
+    //               <button className='mx-auto btn btn-legendary' onClick={loginFunction}>Login / SignUp <i className="fa fa-google" aria-hidden="true"></i></button>
+    //               </div>
+    //             </section>
+    //           }
+    //       </div>
+    //     </div>
+    //   </div>
+    <div className={styles.container}>
+  <div className="row justify-content-center">
+    <div className={`${styles.loginDiv} p-4 col-sm-10 col-md-8 col-lg-6 col-xl-4  rounded-div`}>
+      <div className='d-flex justify-content-center'>
+        <Image src="/logo.svg" className='mx-auto' alt="Vercel Logo" width={250} height={250} />
       </div>
+      {loading && user ? 
+        <section>
+          <div className='d-flex justify-content-center'>
+            <Image src={user.photoURL} className='mx-auto' alt="Qwizard Logo" width={100} height={100} />
+          </div>
+          <div className='d-flex mt-3'>
+            <p className='mx-auto lead'>Welcome, {user.displayName}<br></br>Email: {user.email}</p>
+          </div>
+          <div className='d-flex justify-content-center'>
+            <Image src="/loader.svg" className='mx-auto loader' alt="Profile Logo" width={50} height={50} />
+          </div>
+        </section>
+        :
+        <section>
+          <div className='d-flex'>
+            <p className='lead' style={{fontSize: '20px', fontWeight: 'lighter'}}>Qwizard is a Learning Management System (LMS) which incorporates inside itself:<br />
+              🌎Automatic Question Generation (Short Answer + MCQs)<br />
+              🌎Automatic Solution (Answer) Generation<br />
+              🌎Automatic Quiz Marking.<br />
+              🌎Real-time Quiz Attempting Session for students.
+            </p>
+          </div>
+          <div className='d-flex mt-5 mb-3'>
+            <button className={`${styles.btn} mx-auto btn`} onClick={loginFunction}>Login / SignUp <i className="fa fa-google" aria-hidden="true"></i></button>
+          </div>
+        </section>
+      }
+    </div>
+  </div>
+</div>
   );
 }
 
